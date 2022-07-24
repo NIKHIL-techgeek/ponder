de4573e1c68b9aa1395a1edc0a3f9ddc21b21c0f
Nikhil Kapoor <nikhilkapoor.in@gmail.com>
2022-07-24 00:33:30 -0400

    First Page working

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,16 +1,22 @@
 import React from 'react'
-// import {BrowserRouter as Router , Routes, Route,Link } from 'react-router-dom'
-// import SecondPage from './components/SecondPage.js'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { display } from '@mui/system';
 export default function App() {
   return (
      <div>
         <nav class="main-nav">
             
-        <ButtonGroup variant="contained" aria-label="outlined primary button group">
-        <Button>One</Button>
-        <Button>Two</Button>
-  <     Button>Three</Button>
-        </ButtonGroup>              
+        <Stack spacing={2} direction="row" >
+      <Button variant="text" size="large">home</Button>
+      <Button variant="text" size="large">about</Button>
+      <Button variant="text" size="large">contact</Button>
+    </Stack>            
             
         </nav>
         <section class="top-container">
@@ -19,28 +25,70 @@ export default function App() {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sed minus consequatur odit. Ipsum, quis.</p>
             <a href="#" class="btn">Sample text</a>
         </header>
-        <div class="top-box top-box-a">
-            <h4>membership</h4>
-            <p class="price">100/mo</p>
-            <a href="#" class="btn">buy now</a>
-        </div>
+        
     </section>
-        <a href='/people'>
-                <img alt="Qries" src="https://www.qries.com/images/banner_logo.png"
-                width="150" height="70"/>
-        </a>
-        <a href='/people'>
-                <img alt="Qries" src="https://www.qries.com/images/banner_logo.png"
-                width="150" height="70"/>
-        </a>
-        <a href='/people'>
-                <img alt="Qries" src="https://www.qries.com/images/banner_logo.png"
-                width="150" height="70"/>
-        </a>
-        <a href='/people'>
-                <img alt="Qries" src="https://www.qries.com/images/banner_logo.png"
-                width="150" height="70"/>
-             </a>
+    <a sx={{display:"inline"}}>
+        
+    <a href='/people'>
+    <Card sx={{ maxWidth: 345}} >
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </Card>
+    </a>
+    
+    <a href='/people'>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="../assets/unknown.png"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          BACKYARD HACKS 3 (JULY 29TH - 31ST)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          NORTH AMERICAN TIME ZONE ONLINE
+        </Typography>
+      </CardContent>
+      
+    </Card>
+    </a>
+    <a  href='/people'>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      
+    </Card>
+    </a>
+    </a>
      </div>
   
   )
